c22447b44c6c192dfef3622986f5043985b505e4
msuttt <35849489+msuttt@users.noreply.github.com>
2018-04-13 16:02:36 +0300

    Campaign class eklendi

---
+++ project342/src/project342/Campaign.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+//Author:Mesut Kahraman
+//Date:13.04.2018
+public class Campaign {
+
+	public int Cid;
+	public String title;
+	public String CampaignStartDate;
+	public String CampaignFinishDate;
+	public double estimatedCost;
+	
+	public Campaign( int Id, String t,String csd, String cfd, double estcost) {
+		this.Cid=Id;
+		this.title = t;
+		this.CampaignStartDate = csd;
+		this.CampaignFinishDate = cfd;
+		this.estimatedCost = estcost;
+	}
+	
+	/*public CreateCampaign() {
+		Campaign campaign1=new Campaign(3, "Bilgisayar", "Mayýs", "Haziran", 1500);
+		 ArrayList<Campaign> campaign = new ArrayList<Campaign>();
+		 campaign.add(campaign1);
+		Campaign campaign2=new Campaign(4, "Telefon", "Nisan", "Temmuz", 1300);
+		campaign.add(campaign2);
+		
+	}*/
+}
